58704903e3ad06cb8742fb50393b12b42d5d1089
Victor Mazo <viamazoba@unal.edu.co>
2024-08-22 22:18:29 -0500

    build: Se realiza configuración inicial del proyecto con instalación de dependencias.

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import server from './server'
+import colors from 'colors'
+
+const port = process.env.PORT || 4000
+
+server.listen(port, ()=> {
+  console.log(colors.bgYellow.bold(`Rest API funcionando en el puerto ${port}`.black))
+})
