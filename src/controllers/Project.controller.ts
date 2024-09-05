22a8ffd02d7844cd168fbec38bb5f87cf9d880d0
Victor Mazo <viamazoba@unal.edu.co>
2024-09-04 22:03:01 -0500

    feat: Se crea controller básico para la creación de proyectos.

---
+++ src/controllers/Project.controller.ts
@@ -0,0 +1,20 @@
+import type { Request, Response } from "express"
+import Project from "../models/Project"
+
+
+export class ProjectController {
+  static createProject = async(req: Request, res: Response) => {
+    const project = new Project(req.body)
+
+    try {
+      await project.save() 
+      res.send('Proyecto creado correctamente ...')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  static getAllProjects = async(req: Request, res: Response) => {
+    res.send('Todos los proyectos')
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connetDB } from './config/db'
+import projectRoutes from './routes/projectRoutes'
 
 dotenv.config()
 connetDB()
 const app = express()
+app.use(express.json())
+
+//Routes
+app.use('/api/projects', projectRoutes)
 
 export default app
