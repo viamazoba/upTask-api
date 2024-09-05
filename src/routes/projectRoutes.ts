954155eb753b5daed8078a17f0521ba0141f41f1
Victor Mazo <viamazoba@unal.edu.co>
2024-09-04 22:34:42 -0500

    feat: Se aplican validaciones al endpoint de proyectos.

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { ProjectController } from '../controllers/Project.controller'
+import { handleInputErrors } from '../middleware/validation'
 
 const router = Router()
 
-router.post('/', ProjectController.createProject)
+router.post('/',
+  body('projectName').trim().notEmpty().withMessage('Project\'s name is required'),
+  body('clientName').trim().notEmpty().withMessage('Client\'s name is required'),
+  body('description').trim().notEmpty().withMessage('Description project is required'),
+  handleInputErrors,
+  ProjectController.createProject
+)
 router.get('/', ProjectController.getAllProjects)
 
 export default router
