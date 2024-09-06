5b72174ab919e431c69cc0c306a7a946cfeab312
Victor Mazo <viamazoba@unal.edu.co>
2024-09-05 22:09:02 -0500

    feat: Se crea conexión entre proyectos y tareas.

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,9 +1,11 @@
-import mongoose, {Schema, Document} from "mongoose";
+import mongoose, {Schema, Document, PopulatedDoc, Types} from "mongoose";
+import { ITask } from "./Task";
 
 export interface IProject extends Document  {
   projectName: string
   clientName: string
   description: string
+  task: PopulatedDoc<ITask & Document>[]
 }
 
 const ProjectSchema: Schema = new Schema({
@@ -22,7 +24,13 @@ const ProjectSchema: Schema = new Schema({
     required: true,
     trim: true
   },
-})
+  tasks: [
+    {
+      type: Types.ObjectId,
+      ref: 'Task'
+    }
+  ]
+}, {timestamps: true})
 
 const Project = mongoose.model<IProject>('Project', ProjectSchema)
 
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,9 +1,10 @@
-import mongoose, {Schema, Document} from "mongoose";
+import mongoose, {Schema, Document, Types} from "mongoose";
 
 
 export interface ITask extends Document  {
   name: string
   description: string
+  project: Types.ObjectId
 }
 
 export const TaskSchema : Schema = new Schema({
@@ -16,8 +17,12 @@ export const TaskSchema : Schema = new Schema({
     type: String,
     trim: true,
     require: true
+  },
+  project: {
+    type: Types.ObjectId,
+    ref: 'Project'
   }
-})
+}, {timestamps: true})
 
 const Task = mongoose.model<ITask>('Task', TaskSchema)
 export default Task
