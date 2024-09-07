0e45f93b7fc79d2aeb656162ba7957f905413403
Victor Mazo <viamazoba@unal.edu.co>
2024-09-07 13:11:00 -0500

    feat: Se utiliza método populate para traer información de tareas en los proyectos y viceversa.

--- src/controllers/Task.controller.ts
+++ src/controllers/Task.controller.ts
@@ -24,7 +24,7 @@ export class TaskController {
 
   static getProjectTasks = async (req:Request, res: Response) => {
     try {
-      const tasks = await Task.find({project: req.project.id})
+      const tasks = await Task.find({project: req.project.id}).populate('project')
       res.json(tasks)
     } catch (error) {
       res.status(500).json({error: 'Server Error'})
