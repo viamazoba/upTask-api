588e5346dda2719ebc9c780db5dbae9f845039af
Victor Mazo <viamazoba@unal.edu.co>
2024-09-22 21:55:38 -0500

    feat: instalación y configuración de morgan.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import morgan from 'morgan'
 import { corsConfig } from './config/cors' 
 import { connetDB } from './config/db'
 import projectRoutes from './routes/projectRoutes'
@@ -9,6 +10,11 @@ dotenv.config()
 connetDB()
 const app = express()
 app.use(cors(corsConfig))
+
+// Logging
+app.use(morgan('dev'))
+
+// Leer datos de formulario
 app.use(express.json())
 
 //Routes
