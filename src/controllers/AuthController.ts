9c4adbd5825bc871d30a1dead82fade0c724230f
Victor Mazo <viamazoba@unal.edu.co>
2024-10-04 11:54:55 -0500

    feat: Se implementa validación para usuarios que ya están registrados en la app.

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -7,14 +7,25 @@ export class AuthController {
 
   static createAccount = async (req: Request, res: Response) => {
     try {
-      const { password } = req.body
+
+      const { password, email } = req.body
+
+      // Prevenir duplicados, el usuario ya existe en la db
+      const userExists = await User.findOne({ email })
+
+      if (userExists) {
+        const error = new Error('User already exist')
+        return res.status(409).json({ error: error.message })
+      }
+
+      // Crear usuario
       const user = new User(req.body)
 
       // Hash password
       user.password = await hashPassword(password)
       await user.save()
 
-      res.send('Cuenta creada, revisa tu email para confirmarla')
+      res.send('Account created succesfully, please check your email')
 
     } catch (error) {
       res.status(500).json({ error: 'Hubo un error' })
