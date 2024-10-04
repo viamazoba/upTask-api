131de4d5d97b58f88689caf738886622f5beab20
Victor Mazo <viamazoba@unal.edu.co>
2024-10-04 11:46:30 -0500

    feat: Se implementa encriptación de la contraseña del usuario en su registro.

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,9 +1,23 @@
 import type { Request, Response } from "express"
+import User from "../models/User"
+import { hashPassword } from "../utils/auth"
 
 
 export class AuthController {
 
   static createAccount = async (req: Request, res: Response) => {
-    res.send('desde /api/auth')
+    try {
+      const { password } = req.body
+      const user = new User(req.body)
+
+      // Hash password
+      user.password = await hashPassword(password)
+      await user.save()
+
+      res.send('Cuenta creada, revisa tu email para confirmarla')
+
+    } catch (error) {
+      res.status(500).json({ error: 'Hubo un error' })
+    }
   }
 }
