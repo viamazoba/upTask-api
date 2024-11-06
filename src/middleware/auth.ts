6d924d9030c17d7d814d44e82d64db5938bc42c2
Victor Mazo <viamazoba@unal.edu.co>
2024-11-05 19:03:21 -0500

    feat: Se finaliza implementación del middleware para autenticación del usuario.

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,14 @@
 import { Request, Response, NextFunction } from "express"
+import jwt from "jsonwebtoken"
+import User, { IUser } from "../models/User"
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: IUser
+    }
+  }
+}
 
 export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
   const bearer = req.headers.authorization
@@ -8,6 +18,21 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
   }
 
   const token = bearer.split(' ')[1]
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
+    if (typeof decoded === 'object' && decoded.id) {
+      const user = await User.findById(decoded.id).select('_id name email')
+
+      if (user) {
+        req.user = user
+      } else {
+        res.status(500).json({ error: 'Invalid Token' })
+      }
+    }
+
+  } catch (error) {
+    res.status(500).json({ error: 'Invalid Token' })
+  }
   next()
 }
