6bd0a3e8aa311a6d6690410e5ad77a903d932a03
Victor Mazo <viamazoba@unal.edu.co>
2024-11-10 17:28:07 -0500

    feat: Se crea endpoint para obtener todos los miembros de un equipo.

--- src/controllers/Team.controller.ts
+++ src/controllers/Team.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
+import Project from "../models/Project";
 
 export class TeamMemberController {
 
@@ -14,6 +15,15 @@ export class TeamMemberController {
     }
 
     res.json(user)
+  }
+
+  static getProjectTeam = async (req: Request, res: Response) => {
+    const project = await Project.findById(req.project.id).populate({
+      path: 'team',
+      select: 'id email name'
+    })
+
+    res.json(project.team)
   }
 
   static addMemberById = async (req: Request, res: Response) => {
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -98,6 +98,10 @@ router.post('/:projectId/team/find',
   TeamMemberController.findMemberByEmail
 )
 
+router.get('/:projectId/team',
+  TeamMemberController.getProjectTeam
+)
+
 router.post('/:projectId/team',
   body('id').isMongoId().withMessage('Invalid ID'),
   handleInputErrors,
