d7a73ad703601bd3fba220f25b0b0c9250f65530
Victor Mazo <viamazoba@unal.edu.co>
2024-11-11 20:52:46 -0500

    feat: Se permite la visualización de los proyectos a personas agregadas a un equipo de trabajo.

--- src/controllers/Project.controller.ts
+++ src/controllers/Project.controller.ts
@@ -21,7 +21,8 @@ export class ProjectController {
     try {
       const projects = await Project.find({
         $or: [
-          { manager: { $in: req.user.id } }
+          { manager: { $in: req.user.id } },
+          { team: { $in: req.user.id } }
         ]
       })
       res.json(projects)
@@ -44,7 +45,7 @@ export class ProjectController {
         })
       }
 
-      if (project.manager.toString() !== req.user.id.toString()) {
+      if (project.manager.toString() !== req.user.id.toString() && !project.team.includes(req.user.id)) {
         const error = new Error('Permission denied')
         return res.status(404).json({
           error: error.message
