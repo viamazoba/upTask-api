61eab03bf2f610dc25b5ef5820d1c5db33d049d0
Victor Mazo <viamazoba@unal.edu.co>
2024-11-14 09:06:06 -0500

    feat: Se implementa endpoint para crear notas a una tarea.

--- src/controllers/Note.controller.ts
+++ src/controllers/Note.controller.ts
@@ -4,5 +4,19 @@ import Note, { INote } from '../models/Note'
 export class NoteController {
   static createNote = async (req: Request<{}, {}, INote>, res: Response) => {
     const { content } = req.body
+
+    const note = new Note()
+    note.content = content
+    note.createdBy = req.user.id
+    note.task = req.task.id
+
+    req.task.notes.push(note.id)
+
+    try {
+      await Promise.allSettled([req.task.save(), note.save()])
+      res.send('Note created secessfully')
+    } catch (error) {
+
+    }
   }
 }
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -18,6 +18,7 @@ export interface ITask extends Document {
     user: Types.ObjectId,
     status: TaskStatus
   }[]
+  notes: Types.ObjectId[]
 }
 
 export const TaskSchema: Schema = new Schema({
@@ -52,6 +53,12 @@ export const TaskSchema: Schema = new Schema({
         enum: Object.values(taskStatus),
         default: taskStatus.PENDING
       }
+    }
+  ],
+  notes: [
+    {
+      type: Types.ObjectId,
+      ref: 'Note'
     }
   ]
 }, { timestamps: true })
