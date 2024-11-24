cb304b66891b171d3e6c46d54feccb6cccea1755
Victor Mazo <viamazoba@unal.edu.co>
2024-11-24 14:17:13 -0500

    feat: Se agrega middleware a mongo para eliminar todas las tareas y notas antes de borrar el respectivo proyecto.

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,6 +1,7 @@
 import mongoose, { Schema, Document, PopulatedDoc, Types } from "mongoose";
-import { ITask } from "./Task";
+import Task, { ITask } from "./Task";
 import { IUser } from "./User";
+import Note from "./Note";
 
 export interface IProject extends Document {
   projectName: string
@@ -44,6 +45,18 @@ const ProjectSchema: Schema = new Schema({
     }
   ],
 }, { timestamps: true })
+
+// Middleware (antes hooks)
+/** En este caso se utiliza el middleware para borrar todas las tareas y notas pertenecientes a un proyecto, antes de borrar el proyecto */
+ProjectSchema.pre('deleteOne', { document: true }, async function () {
+  const projectId = this._id
+  if (!projectId) return
+  const tasks = await Task.find({ project: projectId })
+  for (const task of tasks) {
+    await Note.deleteMany({ task: task.id })
+  }
+  await Task.deleteMany({ project: projectId })
+})
 
 const Project = mongoose.model<IProject>('Project', ProjectSchema)
 
